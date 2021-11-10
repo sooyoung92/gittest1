ea5710da483245fbc896d4f620e5384a14da852d
sooyoung92 <plz9chance@gmail.com>
2021-11-10 14:12:01 +0900

    code add2

--- chapter06/src/ch05/Wrapper_Ex3.java
+++ chapter06/src/ch05/Wrapper_Ex3.java
@@ -13,8 +13,8 @@ public class Wrapper_Ex3 {
 		int i = Integer.parseInt(str);
 		short s = Short.parseShort(str);
 		long l = Long.parseLong(str);
-		System.out.println("문자열 byte값 변화 : " + b);
-		System.out.println("문자열 int값 변화 : " + i);
+		System.out.println("문자열 byte값 변환 : " + b);
+		System.out.println("문자열 int값 변환 : " + i);
 		System.out.println("--------------------------");
 		//
 		float f = Float.parseFloat(str2);
@@ -26,27 +26,23 @@ public class Wrapper_Ex3 {
 			System.out.println("boolean 형으로 변경 완료!!!");
 		}
 		System.out.println("--------------------------");
-		
-		// 문제 1
+
+		//
 		String str10 = "1001";
 		String str11 = "2001";
 
-		// str10 과 str11 을 덧셈 연산
-		// 결과값까지 출력
-		// str10 과 str11 을 곱셈 연산
-		// 결과값까지 출력
+		// str10 과 str11 합
+		// str10 과 str11 곱
+		// 결과를
 		int a = Integer.parseInt(str10);
 		int c = Integer.parseInt(str11);
 		int plus = a + c;
 		int gop = a * c;
-		System.out.println("두 String의 합 : " + plus);
-		System.out.println("두 String의 곱 : " + gop);
-	
+		System.out.println("두 String값의 합 : " + plus);
+		System.out.println("두 String값의 곱 : " + gop);
+
 		System.out.println("--------------------------");
-		
-		
-		// 한글깨짐 확인 코드 !!!!!
-		
+
 	}
 
 }
